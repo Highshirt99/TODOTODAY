940cd54796b74c7811420b787511f4dbe1189e1a
Wahab Abdul-Rasheed <84804044+Damilola99-web@users.noreply.github.com>
2023-02-24 14:08:54 +0000

    added the changestatus function

--- src/reduxStore/todoSlice.js
+++ src/reduxStore/todoSlice.js
@@ -10,16 +10,27 @@ export const todoSlice = createSlice({
   name: 'todo',
   initialState,
   reducers: {
-addTodo:(state, action) => {
- state.todos.push(action.payload)
-},
-deleteTodo:(state, action) => {
-  state.todos =  state.todos.filter(item => item.id !== action.payload)
-},
-clearCompletedTodos:(state) => {
-  state.todos = []
-}
-}})
+    addTodo: (state, action) => {
+      state.todos.push({ ...action.payload, status: "active" })
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter(item => item.id !== action.payload)
+    },
+    changeTodoStatus: (state, action) => {
+      const todo = state.todos.filter(item => item.id === action.payload)
+      state.todos = state.todos.filter(item => item.id !== action.payload)
+      if (todo.status === "active") {
+        todo.status = "completed"
+      } else {
+        todo.status = "active"
+      }
+      state.todos.push(todo)
+    },
+    clearCompletedTodos: (state) => {
+      state.todos = []
+    }
+  }
+})
 
 // Action creators are generated for each case reducer function
 export const { addTodo, deleteTodo, clearCompletedTodos } = todoSlice.actions
